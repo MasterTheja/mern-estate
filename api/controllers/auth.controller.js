d2fba06b890c391c05d32f5032e683b8382b5358
MasterTheja <mastertheja@gmail.com>
2024-02-05 13:22:44 +0530

    BE Signin api route addeed in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken"
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -16,4 +17,29 @@ export const signup = async (req, res, next) => {
   }
 };
 
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Wrong Credentials!!")); 
+    }
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials!!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.Jwt_SECRET);
+    const originalUser = {...validUser.toObject()};
+    delete originalUser.password;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(originalUser);
+  } catch (error) {
+    next(error);
+  }
+};
 
