bffede42ee12703cd4b88f18df3632d0b4d7d255
MasterTheja <mastertheja@gmail.com>
2024-02-09 17:48:57 +0530

    FE - Google Auth Added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 function SignUp() {
   const [formData, setFormData] = useState({});
@@ -80,6 +81,7 @@ function SignUp() {
         >
           {loadig ? "loading..." : "Sign up"}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
